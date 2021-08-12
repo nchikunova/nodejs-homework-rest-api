08234f4ccaac638077ed9f457e4dab452b68a8ac
Nataliya <nchikunova86@gmail.com>
2021-08-12 14:53:24 +0300

    hw06- email

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,8 @@ const fs = require('fs/promises')
 const path = require('path')
 const { HttpCode } = require('../services/constants')
 const UploadAvatarService = require('../services/local-upload')
+const EmailService = require('../services/email')
+const CreateSenderSendGrid = require('../services/email-sender')
 
 require('dotenv').config()
 const { SECRET_KEY } = process.env
@@ -19,6 +21,13 @@ const signup = async (req, res, next) => {
       })
     }
     const newUser = await Users.create(req.body)
+    try {
+      const emailService = new EmailService(process.env.NODE_ENV, new CreateSenderSendGrid())
+      await emailService.sendVerifyEmail(newUser.verifyToken, newUser.email)
+    } catch (error) {
+      console.log(error.message)
+    }
+
     return res.status(HttpCode.CREATED).json({
       status: 'success',
       code: HttpCode.CREATED,
@@ -40,7 +49,7 @@ const login = async (req, res, next) => {
     const { email, password } = req.body
     const user = await Users.findByEmail(email)
     const isValidPassword = await user.validPassword(password)
-    if (!user || !isValidPassword) {
+    if (!user || !isValidPassword || !user.isVerified) {
       return next({
         status: HttpCode.UNAUTHORIZED,
         message: 'Email or password is wrong',
@@ -118,13 +127,23 @@ const updateAvatar = async (req, res, next) => {
     const { id } = req.user
     const uploads = new UploadAvatarService('avatars')
     const avatarUrl = await uploads.saveAvatar({ idUser: id, file: req.file })
-    console.log('🚀 ~ file: users.js ~ line 121 ~ updateAvatar ~ avatarUrl', req.user.avatarURL)
+    console.log(
+      '🚀 ~ file: users.js ~ line 121 ~ updateAvatar ~ avatarUrl',
+      req.user.avatarURL,
+    )
 
     try {
       await fs.unlink(path.join('public', req.user.avatarURL))
-      console.log("🚀 ~ file: users.js ~ line 125 ~ updateAvatar ~ 'public', req.user.avatarURL", 'public', req.user.avatarURL)
+      console.log(
+        '🚀 ~ file: users.js ~ line 125 ~ updateAvatar ~ "public", req.user.avatarURL',
+        'public',
+        req.user.avatarURL,
+      )
     } catch (e) {
-      console.log('🚀 ~ file: users.js ~ line 127 ~ updateAvatar ~ error', e.message)
+      console.log(
+        '🚀 ~ file: users.js ~ line 127 ~ updateAvatar ~ error',
+        e.message,
+      )
     }
 
     await Users.updateAvatar(id, avatarUrl)
@@ -138,11 +157,71 @@ const updateAvatar = async (req, res, next) => {
   }
 }
 
+const verify = async (req, res, next) => {
+  try {
+    const user = await Users.findByVerifyToken(req.params.verificationToken)
+    if (user) {
+      await Users.updateTokenVerify(user.id, true, null)
+      return res.json({
+        status: 'success',
+        code: HttpCode.OK,
+        message: 'Verification successful'
+      })
+    }
+    return res
+      .status(HttpCode.NOT_FOUND)
+      .json({
+        status: 'error',
+        code: HttpCode.NOT_FOUND,
+        message: 'User not found'
+      })
+  } catch (error) {
+    next(error)
+  }
+}
+
+const repeatEmailVerification = async (req, res, next) => {
+  try {
+    const user = await Users.findByEmail(req.body.email)
+    if (user) {
+      const { isVerified, verifyToken, email } = user
+
+      if (!isVerified) {
+        const emailService = new EmailService(process.env.NODE_ENV, new CreateSenderSendGrid())
+        await emailService.sendVerifyEmail(verifyToken, email)
+        return res.json({
+          status: 'success',
+          code: HttpCode.OK,
+          data: {
+            message: 'Verification email sent'
+          },
+        })
+      }
+
+      return res.status(HttpCode.CONFLICT).json({
+        status: 'error',
+        code: HttpCode.CONFLICT,
+        message: 'Verification has already been passed',
+      })
+    }
+
+    return res.status(HttpCode.NOT_FOUND).json({
+      status: 'error',
+      code: HttpCode.NOT_FOUND,
+      message: 'User not found',
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   signup,
   login,
   logout,
   currentUser,
   updateSub,
-  updateAvatar
+  updateAvatar,
+  verify,
+  repeatEmailVerification
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { validateAuth, validateUpdateSub } = require('../../services/validation')
+const { validateAuth, validateUpdateSub, schemaRepeatVerificated } = require('../../services/validation')
 const userController = require('../../controllers/users')
 const guard = require('../../services/guard')
 const multer = require('../../services/multer')
@@ -9,8 +9,12 @@ router
   .post('/signup', validateAuth, userController.signup)
   .post('/login', validateAuth, userController.login)
   .post('/logout', guard, userController.logout)
+
   .get('/current', guard, userController.currentUser)
   .patch('/', guard, validateUpdateSub, userController.updateSub)
   .patch('/avatars', guard, multer.single('avatar'), userController.updateAvatar)
+
+  .get('/verify/:verificationToken', userController.verify)
+  .post('/verify', schemaRepeatVerificated, userController.repeatEmailVerification)
 
 module.exports = router
